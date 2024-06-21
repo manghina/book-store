d455d9ac73b7b9f78b3b003806603198f35a0ae5
Dario <webclosureinfo@gmail.com>
2024-06-21 11:55:58 +0200

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+
 import './bootstrap';
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+const app = createApp({});
+app.component('dashboard', Dashboard);
+app.mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::get('/', function () {
     return response()->json(['success' => 'false'], 401);
 })->name('login');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
